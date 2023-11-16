0b18c731a270210ce8dde778016f5e68d9a0832e
Sadeghi <145283167+Sir-sadeghi@users.noreply.github.com>
2023-11-16 08:57:57 +0330

    Add files via upload

---
+++ OnlineHacking/logo.sh
@@ -0,0 +1,50 @@
+clear
+echo -e "\e[95m███╗   ███╗███╗   ███╗██████╗
+        ████╗ ████║████╗ ████║██╔══██╗
+        ██╔████╔██║██╔████╔██║██║  ██║
+        ██║╚██╔╝██║██║╚██╔╝██║██║  ██║
+        ██║ ╚═╝ ██║██║ ╚═╝ ██║██████╔╝
+        ╚═╝     ╚═╝╚═╝     ╚═╝╚═════╝
+  \e[95;1m \e[0m\n"
+    echo""    
+  echo -e $'\e[1;33m\e[0m\e[1;31m    ██████████\e[0m'"\e[1;37m██████████"'\e[1;33m\e[0m\e[0;32m██████████\e[0m' '\e[1;32m\e[0m\e[1;32m Camera Hacking \e[0m''\e[1;37m\e[0m\e[1;37m [v 2.1] \e[0m'                                       
+  echo ""
+   echo -e $'\e[1;33m\e[0m\e[1;33m  [\e[0m\e[1;32m Follow Github : \e[36mhttps://github.com/sir-sadeghi/CameraHack\e[0m \e[1;32m\e[0m\e[1;33m]\e[0m'
+   echo ""
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[93m    |M|o|h|m|a|d| |S|a|d|e|g|h|i|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ >>\e[0m' 
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m'
+      echo -e "\e[95m    |M|A|M|M|D|"      
+      echo -e $'\e[1;37m\e[0m\e[1;37m    +-+-+-+-+-+ >>\e[0m' 
+      echo "" 
+sleep 8
+clear
+xdg-open "https://rubika.ir/coding_maestro"
+echo ""
+echo ""
+printf "\e[31m[\e[32m★\e[31m]\e[32m This Tool is only educational purpose and we are not responsible for kind of illegal activity done by this tool\e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Accepted This Notice \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+
+clear
+
+
+echo ""
+printf "\e[100;330m[\e[10m **** ]\e[1;40m\e[10m sir-sadeghi :\e[1;32m Join Telegram usename \e[1;33m @Lockader_virus  !\e[0m"
+sleep 4
+echo ""
+clear
